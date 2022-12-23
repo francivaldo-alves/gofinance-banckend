58744779b62230b7dbee2c6d85186ccd098fcaac
Francivaldo <francivaldo.alves@gmail.com>
2022-12-22 21:02:32 -0300

    feat: create api to category

---
+++ api/category.go
@@ -0,0 +1,142 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	db "github.com/francivaldo-alves/gofinance-bankend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createCategoryRequest struct {
+	UserID      int32  `json:"user_id" binding:"required"`
+	Title       string `json:"title" binding:"required"`
+	Type        string `json:"type" binding:"required"`
+	Description string `json:"description" binding:"required"`
+}
+
+func (server *Server) createCategory(ctx *gin.Context) {
+
+	var req createCategoryRequest
+	err := ctx.ShouldBindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	arg := db.CreateCategoryParams{
+		UserID:      req.UserID,
+		Title:       req.Title,
+		Type:        req.Type,
+		Description: req.Description,
+	}
+
+	category, err := server.store.CreateCategory(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, category)
+}
+
+// Funcação da PI para buscar uma category
+type getCategoryRequest struct {
+	ID int32 `uri:"id" binding:"required"`
+}
+
+func (server *Server) getCategory(ctx *gin.Context) {
+
+	var req getCategoryRequest
+	err := ctx.ShouldBindUri(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	category, err := server.store.GetCategory(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, category)
+}
+
+type getCategoriesRequest struct {
+	UserID      int32  `json:"user_id" binding:"required" `
+	Type        string `json:"type" binding:"required"`
+	Title       string `json:"title" `
+	Description string `json:"description"`
+}
+
+// Funcação da PI para buscar uma category
+func (server *Server) getCategories(ctx *gin.Context) {
+	var req getCategoriesRequest
+	err := ctx.ShouldBindUri(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+	arg := db.GetCategoriesParams{
+		UserID:      req.UserID,
+		Title:       req.Title,
+		Type:        req.Type,
+		Description: req.Description,
+	}
+
+	categories, err := server.store.GetCategories(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, categories)
+}
+
+type updateCategoryRequest struct {
+	ID          int32  `json:"id" binding: "required`
+	Title       string `json:"title" `
+	Description string `json:"description" `
+}
+
+// Funcação da PI para cadastar uma categoria
+func (server *Server) updateCategory(ctx *gin.Context) {
+	var req updateCategoryRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+
+	}
+	arg := db.UpdateCategoryParams{
+		ID:          req.ID,
+		Title:       req.Title,
+		Description: req.Description,
+	}
+	category, err := server.store.UpdateCategory(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	ctx.JSON(http.StatusOK, category)
+}
+
+type deleteCategoryByRequest struct {
+	ID int32 `uri:"id" binding:"required"`
+}
+
+// Funcação da PI para deletar uma categoria
+func (server *Server) deleteCategory(ctx *gin.Context) {
+	var req deleteCategoryByRequest
+	err := ctx.ShouldBindUri(&req)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+	err = server.store.DeleteCategory(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, true)
+}
