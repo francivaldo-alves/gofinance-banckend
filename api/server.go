3b6ab353daca303568eb38d335954d5a4c13e1c8
Francivaldo <francivaldo.alves@gmail.com>
2022-12-24 06:13:37 -0300

    add news routers

--- api/server.go
+++ api/server.go
@@ -14,11 +14,25 @@ func NewServer(store *db.SQLStore) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	//rotas URI da API
+	//rotas URI da API USER
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
 	router.GET("/user/id/:id", server.getUserById)
 
+	//rotas URI da API Category
+	router.POST("/category", server.createCategory)
+	router.GET("/category/id/:id", server.getCategory)
+	router.GET("/category", server.getCategories)
+	router.PUT("/category/:id", server.updateCategory)
+	router.DELETE("/category/:id", server.deleteCategory)
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account/id/:id", server.getAccount)
+	router.GET("/account/graph/:user_id/:type", server.getAccountGraph)
+	router.GET("/account/reports/:user_id/:type", server.getAccountRports)
+	router.DELETE("/account/:id", server.deleteAccount)
+	router.PUT("/account/:id", server.updateAccount)
+	router.GET("/account", server.getAccounts)
 	server.router = router
 	return server
 }
